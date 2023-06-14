1af132f92818e2a754a35356a44379ccdfe833e5
engNesta <robert.nuhu0002@stud.hkr.se>
2023-06-14 08:05:22 +0200

    Designed Gui

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -14,8 +14,18 @@ MainComponent::~MainComponent()
 void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    juce::Colour myColour = juce::Colour::fromRGB(18, 18, 18);
-    g.fillAll (myColour);
+    juce::Colour offBlack = juce::Colour::fromRGB(18, 18, 18);
+    g.fillAll (offBlack);
+
+    juce::Colour offWhite = juce::Colour::fromRGB(250, 249, 246);
+    g.setColour(offWhite);
+
+    g.drawLine(150, 0, 155, 600);
+    g.drawLine(450, 0, 455, 600);
+
+
+    //get my colour 250,249,246
+    //draw 2 vertical lines on the left and right
 
 }
 
